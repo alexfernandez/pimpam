890a980723425cfbec9e309bc6ea5ead87cccceb
Alex Fernández <alex.fernandez@mediasmart.es>
2013-02-16 17:23:19 +0100

    Added everything.

---
+++ pimpam.js
@@ -0,0 +1,16 @@
+'use strict.js'
+
+var expect = require('expect.js');
+
+
+exports.pimpam = pimpam();
+
+function pimpam() {
+}
+
+function testPimpam() {
+	expect(pimpam(1)).to.eql('');
+}
+
+testPimpam();
+
